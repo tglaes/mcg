6a61cfd5389a79de474e97134d30bb6b11efbcfe
Tristan Glaes <trglae@gmail.com>
2022-01-29 16:52:01 +0100

    Implemented algorithm for csr format without openMP pragmas

--- jacobi-open_mp/main.c
+++ jacobi-open_mp/main.c
@@ -1,7 +1,27 @@
 // gcc main.c read_matrix_csr.c -o main -fopenmp
 #include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
+#include <string.h>
+#include <math.h>
 #include "read_matrix_csr.h"
+
+int checkIteration(float *x, float *y);
+void evaluateSolution();
+void printData();
+void freeResources();
+
+int dimension = 0;
+int data_size = 0;
+int row_ptr_size = 0;
+float *data = NULL;
+int *cols = NULL;
+int *row_ptr = NULL;
+float *x;
+float *y;
+float *vector = NULL;
+int k;
+float EPSILON = 0.0001;
 
 int main(int argc, char *argv[])
 {
@@ -13,16 +33,83 @@ int main(int argc, char *argv[])
 		printf("You need to provide a matrix as argument!\n");
 		return 1;
 	}
-
-	int dimension = 0;
-	int data_size = 0;
-	int row_ptr_size = 0;
-	float *data = NULL;
-	int *cols = NULL;
-	int *row_ptr = NULL;
-	float *vector = NULL;
 	dimension = readMatrixAndVectorFromFile(argv[1], &data, &cols, &row_ptr, &vector, &data_size, &row_ptr_size);
+	float *x = calloc(dimension, sizeof(float));
+	float *y = calloc(dimension, sizeof(float));
 
+	double time_t = omp_get_wtime();
+
+	// Schleife für die Iterationen
+	for (k = 1; k < 1000; k++)
+	{
+		for (int i = 0; i < row_ptr_size - 1; i++)
+		{
+			float row_result = 0;
+			int index_of_diagonal_element;
+			for (int j = row_ptr[i]; j < row_ptr[i + 1]; j++)
+			{
+				//printf("%d ", j);
+				//printf("%f ", data[j]);
+				// i und cols[j] zusammen sind das Matrixelement matrix[i][cols[j]]
+				if (i != cols[j])
+				{
+					row_result -= (data[j] * x[i]);
+				}
+				else
+				{
+					row_result += vector[i];
+					// Der Index des Diagonalelements der aktuellen Zeile (Zeile i) im data Array
+					index_of_diagonal_element = j;
+					//printf("%d\n", j);
+				}
+			}
+
+			// Teilen durch das Diagonalelement
+			y[i] = row_result / data[index_of_diagonal_element];
+			//printf("\n");
+		}
+
+		// Überprüfen, ob sich ein Wert mehr als EPSILON verändert hat
+		int iterationCheck = checkIteration(x, y);
+		memcpy(x, y, sizeof(float) * dimension);
+		if (iterationCheck != 0)
+		{
+			break;
+		}
+		//printf("%f\n", x[0]);
+	}
+
+	double time_delta = omp_get_wtime() - time_t;
+	printf("Computation finished after %d iteration(s) and took %f seconds\n", k, time_delta);
+
+	for (int i = 0; i < dimension; i++)
+	{
+		printf("%f\n", y[i]);
+	}
+
+	freeResources();
+}
+
+int checkIteration(float *x, float *y)
+{
+	for (int i = 0; i < dimension; i++)
+	{
+		if (fabs((fabs(x[i]) - fabs(y[i]))) > EPSILON)
+		{
+			//printf("%d ", i);
+			//printf("%f, %f, %f\n", fabs(x[i]), fabs(y[i]), fabs(x[i]) - fabs(y[i]));
+			return 0;
+		}
+	}
+	return 1;
+}
+
+void evaluateSolution()
+{
+}
+
+void printData()
+{
 	for (int k = 0; k < data_size; k++)
 	{
 		printf("%f,", data[k]);
@@ -46,4 +133,14 @@ int main(int argc, char *argv[])
 		printf("%f,", vector[k]);
 	}
 	printf("\n");
+}
+
+void freeResources()
+{
+	free(data);
+	free(cols);
+	free(row_ptr);
+	free(x);
+	free(y);
+	free(vector);
 }
--- shared/matrix-generation/matrixGen.js
+++ shared/matrix-generation/matrixGen.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 let outputFileName = "matrix.csv";
 
 // The dimension of the matrix and the vector
-const matrixDimension = 7;
+const matrixDimension = 5;
 // The minimal value of an element on the diagonal
 const minValueDiagonalElement = 100;
 // The maximal value of an element on the diagonal
@@ -11,14 +11,14 @@ const maxValueDiagonalElement = 200;
 // The maximal value of an element which is not on the diagonal
 const maxValueNonDiagonalElement = 10;
 // The amount of element per row unequal to zero (How many numbers unequal to zero should the row have)
-let NUMBER_OF_NON_ZERO_ENTRIES_PER_ROW = 4;
+let NUMBER_OF_NON_ZERO_ENTRIES_PER_ROW = 2;
 // The amount of elements unequal to zero is NUMBER_OF_NON_ZERO_ENTRIES_PER_ROW + 1 (beacuse of the element on the diagonal)
 
 let CHANCE_FOR_ZERO_ELEMENT_IN_ELL = 0.5;
 
 // COO rows
 // The total number of COO rows in the matrix (set to zero to not use COO rows)
-let NUMBER_OF_COO_ROWS = 2;
+let NUMBER_OF_COO_ROWS = 0;
 // The amount of numbers unequal to zero in a COO row
 let NUMBER_OF_NON_ZERO_ENTRIES_IN_COO_ROW = 6;
 // An array with the indicies of COO rows
@@ -35,8 +35,8 @@ generateVector();
 //printVectorAndMatrix();
 //testMatrixAndVector();
 //writeMatrixAndVectorToFile();
-//writeMatrixAndVectorToFileCsr();
-writeMatrixAndVectorToFileEllCoo();
+writeMatrixAndVectorToFileCsr();
+//writeMatrixAndVectorToFileEllCoo();
 
 function generateMatrix() {
 
